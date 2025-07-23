2e617973e72a4ca94583e5baac827e213a88cf99
Meet Patel <meetpatel@Meets-MacBook-Air.local>
2025-07-23 16:02:20 +0530

    Ready for EB deploy

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = 'portfolio.urls'
 
 TEMPLATES = [
